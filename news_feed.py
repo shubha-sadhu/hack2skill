ac32a86ff30b65124c7d4e1d7851922ede6c5770
Samriddha Sahu <229734442+samsahu2007@users.noreply.github.com>
2026-04-17 00:59:57 +0530

    deleted previous csv file being used and removed news_feed.py

--- news_feed.py
+++
@@ -1,8 +0,0 @@
-import feedparser
-
-url = "https://news.google.com/rss/search?q=supply+chain+disruption+OR+logistics+delay+when:1d"
-
-feed = feedparser.parse(url)
-
-for entry in feed.entries:
-    print(entry.title)
